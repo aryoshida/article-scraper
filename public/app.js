dd71bec44991d8662e86627dbc29e93c4ddc4c1d
Andrea Yoshida <aryoshida9340@gmail.com>
2019-07-02 17:18:51 -0600

    got the basics to work well

--- public/app.js
+++ public/app.js
@@ -1,26 +1,22 @@
 $.getJSON("/articles", function(data){
     for (var i = 0; i < data.length; i++){
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-        // $("#articles").append(
-        //     "<p data-id='" + data[i]._id + "'>"
-        //     + data[i].title + "<br/>"
-        //     + data[i].link + "</p>");
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /> &nbsp &nbsp &nbsp &nbsp" + data[i].link + "</p> <br />");
     }
 });
 
 $(document).on("click", "p", function(){
     $("#notes").empty();
-    var clickedId = $(this).attr("data-id");
+    var thisId = $(this).attr("data-id");
 
     $.ajax({
         method: "GET",
-        url: "/articles/" + clickedId
+        url: "/articles/" + thisId
     }).then(function(data){
         console.log(data);
         $("#notes").append("<h2>" + data.title + "</h2>");
         $("#notes").append("<input id='titleinput' name='title' >");
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        $("#notes").append("<button data-id='" + data_id + "' id='savenote'>Save Article</button>");
+        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
         if(data.note){
             $("#titleinput").val(data.note.title);
@@ -30,11 +26,11 @@ $(document).on("click", "p", function(){
 });
 
 $(document).on("click", "#savenote", function(){
-    var clickedId = $(this).attr("data-id");
+    var thisId = $(this).attr("data-id");
 
     $.ajax({
         method: "POST",
-        url: "/article/" + clickedId,
+        url: "/article/" + thisId,
         data: {
             title: $("#titleinput").val(),
             body: $("#bodyinput").val()
--- server.js
+++ server.js
@@ -21,48 +21,24 @@ app.listen(PORT, function(){
 });
 
 //scrapes articles off of the website
-app.get("/scrape", function(req, res){
-    axios.get("https://www.reddit.com/").then(function(response){
-        var $ = cheerio.load(response.data);
-
-        $("article div").each(function(i, element){
-            var result = {};
-
-            var linkObject = $(this).children("div");
-            var child1 = $(linkObject).children("div");
-            var child2 = $(child1).children("a");
-            var child3 = $(child2).children("div")
-
-            result.title = child3.children("h3").text();
-            result.link = child2.attr("href");
-
-            db.Article.create(result)
-                .then(function(dbArticle){
-                    // console.log(dbArticle);
-                })
-                .catch(function(err){
-                    console.log(err);
-                });
-        });
-        res.send("Scrape Completed!!!");
-    });
-});
-
 // app.get("/scrape", function(req, res){
-//     axios.get("https://www.nytimes.com/").then(function(response){
+//     axios.get("https://www.reddit.com/").then(function(response){
 //         var $ = cheerio.load(response.data);
 
 //         $("article div").each(function(i, element){
 //             var result = {};
 
-//             var linkObject = $(this).children("a");
+//             var linkObject = $(this).children("div");
 //             var child1 = $(linkObject).children("div");
-//             var header = $(child1).children("h2");
-//             result.title = header.text();
+//             var child2 = $(child1).children("a");
+//             var child3 = $(child2).children("div")
+
+//             result.title = child3.children("h3").text();
+//             result.link = child2.attr("href");
 
 //             db.Article.create(result)
 //                 .then(function(dbArticle){
-//                     console.log(dbArticle);
+//                     // console.log(dbArticle);
 //                 })
 //                 .catch(function(err){
 //                     console.log(err);
@@ -71,6 +47,33 @@ app.get("/scrape", function(req, res){
 //         res.send("Scrape Completed!!!");
 //     });
 // });
+
+app.get("/scrape", function(req, res){
+    axios.get("https://www.foxnews.com/").then(function(response){
+        var $ = cheerio.load(response.data);
+
+        $("article div header").each(function(i, element){
+            var result = {};
+
+            var linkObject = $(this).children("h2");
+            var child1 = $(linkObject).children("a");
+
+            result.title = child1.text();
+            result.link = child1.attr("href");
+            
+            db.Article.create(result)
+                .then(function(dbArticle){
+                    console.log(dbArticle);
+                })
+                .catch(function(err){
+                    console.log(err);
+                });
+        });
+
+        res.send("Scrape Complete");
+
+    });
+});
 
 //grabs all articles
 app.get("/articles", function(req, res){
@@ -98,13 +101,13 @@ app.get("/articles/:id", function(req, res){
 //adds selected article to saved 
 app.post("articles/:id", function(req, res){
     db.Note.create(req.body)
-    .then(function(dbNote){
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle){
-        res.json(dbArticle);
-    })
-    .catch(function(err){
-        res.json(err);
-    });
+        .then(function(dbNote){
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function(dbArticle){
+            res.json(dbArticle);
+        })
+        .catch(function(err){
+            res.json(err);
+        });
 });
